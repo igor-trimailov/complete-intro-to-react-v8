3b3fde78a5b3e0ff2e20cf8f4504954cd8c8d101
Igor Trimailov <itrimailov@intermedia.com>
2024-02-16 14:28:30 +0000

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const queryClient = new QueryClient({
 const App = () => {
     return (
         <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
+            <QueryClientProvider client={queryClient }>
                 <header>
                     <Link to="/">Adopt Me!</Link>
                 </header>
